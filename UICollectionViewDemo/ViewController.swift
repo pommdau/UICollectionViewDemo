7477f0214ed0d38c96da8e92e05b554df242e2b3
hikeuchi <hikeuchi@morisawa.co.jp>
2020-11-12 09:19:04 +0900

    initial commit

---
+++ UICollectionViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViewDemo
+//
+//  Created by HIROKI IKEUCHI on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
