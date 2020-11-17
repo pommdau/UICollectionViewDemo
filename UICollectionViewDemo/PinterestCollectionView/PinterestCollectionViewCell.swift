4fe4ccfcaf84d6ee9e9a147034645193016d245b
hikeuchi <hikeuchi@morisawa.co.jp>
2020-11-17 14:23:51 +0900

    クラスのファイルだけ用意

---
+++ UICollectionViewDemo/PinterestCollectionView/PinterestCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PinterestCollectionViewCell.swift
+//  UICollectionViewDemo
+//
+//  Created by HIROKI IKEUCHI on 2020/11/17.
+//
+
+import Foundation
---
+++ UICollectionViewDemo/PinterestCollectionView/PinterestCollectionViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PinterestCollectionViewController.swift
+//  UICollectionViewDemo
+//
+//  Created by HIROKI IKEUCHI on 2020/11/17.
+//
+
+import Foundation
