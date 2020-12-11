7d848d2033036f31ed1ff76b6fb5dd668dfb3dbd
hikeuchi <hikeuchi@morisawa.co.jp>
2020-12-12 03:48:29 +0900

    画像の大きさ計算をviewModelに分離

--- UICollectionViewDemo/DiffableDatasourceDemo_2/TweetCell.swift
+++ UICollectionViewDemo/DiffableDatasourceDemo_2/TweetCell.swift
@@ -105,72 +105,45 @@ class TweetCell: UICollectionViewCell {
     // MARK: - Helpers
     
     private func configureUI() {
-        
         guard let tweet = tweet else { return }
-        
-        let imageStack: UIStackView
-        
-        switch tweet.images.count {
-        case 1:
-            imageStack = UIStackView(arrangedSubviews: [mediaImageView_01])
-        case 2:
-            imageStack = UIStackView(arrangedSubviews: [mediaImageView_01, mediaImageView_02])
-            imageStack.axis = .horizontal
-            imageStack.distribution = .fillEqually
-            imageStack.spacing = 4
-        case 3:
-            let rightImageStack = UIStackView(arrangedSubviews: [mediaImageView_02, mediaImageView_03])
-            rightImageStack.axis = .vertical
-            rightImageStack.distribution = .fillEqually
-            rightImageStack.spacing = 4
-
-            imageStack = UIStackView(arrangedSubviews: [mediaImageView_01, rightImageStack])
-            imageStack.axis = .horizontal
-            imageStack.distribution = .fillEqually
-            imageStack.spacing = 4
-        case 4:
-            print("DEBUG: 🍏")
-        default:
-            fatalError()
-        }
-        
-//        imageStack.setDimensions(width: self.frame.width, height: self.frame.width)
-        let imageView01Size = calculateImageSize(withImage: tweet.images[0])
-        let imageVIew02and03Sizes = calculateTwoImageSizes(withImages: [tweet.images[1], tweet.images[2]])
-        let imageView02Size = imageVIew02and03Sizes[0]
-        let imageView03Size = imageVIew02and03Sizes[1]
-        let imageView04Size = calculateImageSize(withImage: tweet.images[3])
-        mediaImageView_01.setDimensions(width: imageView01Size.width,
-                                        height: imageView01Size.height)
-        mediaImageView_02.setDimensions(width: imageView02Size.width,
-                                        height: imageView02Size.height)
-        mediaImageView_03.setDimensions(width: imageView03Size.width,
-                                        height: imageView03Size.height)
-        mediaImageView_04.setDimensions(width: imageView04Size.width,
-                                        height: imageView04Size.height)
         
         addSubview(mediaImageView_01)
         addSubview(mediaImageView_02)
         addSubview(mediaImageView_03)
         addSubview(mediaImageView_04)
         
-        mediaImageView_01.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor)
-        mediaImageView_02.anchor(top: mediaImageView_01.bottomAnchor, left: leftAnchor)
-        mediaImageView_03.anchor(top: mediaImageView_01.bottomAnchor, left: mediaImageView_02.rightAnchor,
-                                 right: rightAnchor)
-        mediaImageView_04.anchor(top: mediaImageView_02.bottomAnchor, left: leftAnchor, right: rightAnchor)
+        var imageSizes: [CGSize] = [.zero, .zero, .zero, .zero]
         
-        /*
-        let actionStack = UIStackView(arrangedSubviews: [addListButton, retweetButton, likeButton])
-        actionStack.axis = .horizontal
-        actionStack.distribution = .fillEqually
-        actionStack.spacing = 2
-        addSubview(actionStack)
-//        actionStack.anchor(top: imageStack.bottomAnchor, left: leftAnchor,bottom: bottomAnchor, right: rightAnchor,
-//                           paddingTop: 4)
-        actionStack.anchor(top: imageStack.bottomAnchor, left: leftAnchor, right: rightAnchor,
-                           paddingTop: 4)
-        */
+        switch tweet.images.count {
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+        case 4:
+            let viewModel = TweetCellViewModel(tweet: tweet)
+            imageSizes = viewModel.calculateImageSizes(withCellWidth: self.frame.width)
+            
+            mediaImageView_01.anchor(top: topAnchor, left: leftAnchor,
+                                     right: rightAnchor)
+            mediaImageView_02.anchor(top: mediaImageView_01.bottomAnchor, left: leftAnchor)
+            mediaImageView_03.anchor(top: mediaImageView_01.bottomAnchor, left: mediaImageView_02.rightAnchor,
+                                     right: rightAnchor)
+            mediaImageView_04.anchor(top: mediaImageView_02.bottomAnchor, left: leftAnchor,
+                                     right: rightAnchor)
+        default:
+            break
+        }
+        
+        mediaImageView_01.setDimensions(width: imageSizes[0].width,
+                                        height: imageSizes[0].height)
+        mediaImageView_02.setDimensions(width: imageSizes[1].width,
+                                        height: imageSizes[1].height)
+        mediaImageView_03.setDimensions(width: imageSizes[2].width,
+                                        height: imageSizes[2].height)
+        mediaImageView_04.setDimensions(width: imageSizes[3].width,
+                                        height: imageSizes[3].height)
         
         configureImageViewContent()
     }
@@ -194,75 +167,6 @@ class TweetCell: UICollectionViewCell {
             mediaImageView_04.image = tweet.images[3]
         }
     }
-}
-
-extension TweetCell {
-    
-    func calculateImageSize(withImage image: UIImage) -> CGSize {
-
-        let widthOfView = self.frame.width
-        let ratio = widthOfView / image.size.width
-        
-        return CGSize(width: widthOfView, height: image.size.height * ratio)
-    }
-    
-    func calculateTwoImageSizes(withImages images: [UIImage]) -> [CGSize] {
-//        let netWidth = view.frame.width - imageInsetsConstant * 2  // いるのかわからん
-        let netWidth = self.frame.width
-        
-        let size1 = images[0].size
-        let size2 = images[1].size
-        
-        // 高さを揃えて幅の割合を計算
-        let w2_dash = size1.width * (size1.height / size2.height)
-        let w2_dash_dash = netWidth * (w2_dash / (size1.width + w2_dash))
-        let resultHeight = size2.height * (w2_dash_dash / size2.width)
-        
-//        let w1_dash_dash = netWidth * (size1.width / (size1.width + w2_dash))
-        let w1_dash_dash = netWidth - w2_dash_dash
-        
-        return [CGSize(width: w1_dash_dash, height: resultHeight),
-                CGSize(width: w2_dash_dash, height: resultHeight)]
-    }
-    
-    
-//    func calculateWidthFraction(withImages images: [UIImage], atIndex index: Int) -> CGFloat {
-//        var sizes = [CGSize]()
-//        images.forEach { image in
-//            sizes.append(sizeAspectFittingView(image: image))
-//        }
-//
-//        return sizes[index].width / sizes.reduce(0, { result, element in result + element.width })
-//    }
-//
-//    func calculateHeightFraction(withImages images: [UIImage], atIndex index: Int) -> CGFloat {
-//        var sizes = [CGSize]()
-//        images.forEach { image in
-//            sizes.append(sizeAspectFittingView(image: image))
-//        }
-//
-//        return sizes[index].height / sizes.reduce(0, { result, element in result + element.height })
-//    }
-    
-    func sizeAspectFittingView(image: UIImage) -> CGSize {
-        let widthOfView = self.frame.width
-        let heightOfView = self.frame.height
-        let widthOfImage = image.size.width
-        let heightOfImage = image.size.height
-        
-        guard widthOfView != 0,
-              heightOfView != 0,
-              widthOfImage != 0,
-              heightOfImage != 0 else { return CGSize(width: 100, height: 100) }
-        
-        let ratio = widthOfView / widthOfImage
-
-        let newWidthOfImage = widthOfView - imageInsetsConstant * 2
-        let newHeightOfImage = heightOfImage * ratio - imageInsetsConstant * 2
-        
-        return CGSize(width: newWidthOfImage, height: newHeightOfImage)
-    }
-    
 }
 
 class TweetMediaTapGestureRecognizer: UITapGestureRecognizer {
---
+++ UICollectionViewDemo/DiffableDatasourceDemo_2/TweetCellViewModel.swift
@@ -0,0 +1,81 @@
+//
+//  TweetCellViewModel.swift
+//  UICollectionViewDemo
+//
+//  Created by HIROKI IKEUCHI on 2020/12/12.
+//
+
+
+import UIKit
+
+struct TweetCellViewModel {
+    
+    // MARK: - Properties
+    
+    let tweet: Tweet
+    
+    private let imageInsetsConstant: CGFloat = 2
+    
+    // MARK: - Lifecycle
+    
+    init(tweet: Tweet) {
+        self.tweet = tweet
+    }
+    
+    // MARK: - Helpers
+    
+    func calculateImageSizes(withCellWidth width: CGFloat) -> [CGSize] {
+        let size0     = calculateImageSize(withImage: tweet.images[0], width: width)
+        let size1and2 = calculateTwoImageSizes(withImages: [tweet.images[1], tweet.images[2]], width: width)
+        let size3     = calculateImageSize(withImage: tweet.images[3], width: width)
+        return [size0, size1and2[0], size1and2[1], size3]
+    }
+    
+    
+    private func calculateImageSize(withImage image: UIImage, width: CGFloat) -> CGSize {
+
+        let widthOfView = width
+        let ratio = widthOfView / image.size.width
+        
+        return CGSize(width: widthOfView, height: image.size.height * ratio)
+    }
+    
+    private func calculateTwoImageSizes(withImages images: [UIImage], width: CGFloat) -> [CGSize] {
+//        let netWidth = view.frame.width - imageInsetsConstant * 2  // いるのかわからん
+        let netWidth = width
+        
+        let size1 = images[0].size
+        let size2 = images[1].size
+        
+        // 高さを揃えて幅の割合を計算
+        let w2_dash = size1.width * (size1.height / size2.height)
+        let w2_dash_dash = netWidth * (w2_dash / (size1.width + w2_dash))
+        let resultHeight = size2.height * (w2_dash_dash / size2.width)
+        
+//        let w1_dash_dash = netWidth * (size1.width / (size1.width + w2_dash))
+        let w1_dash_dash = netWidth - w2_dash_dash
+        
+        return [CGSize(width: w1_dash_dash, height: resultHeight),
+                CGSize(width: w2_dash_dash, height: resultHeight)]
+    }
+    
+    private func sizeAspectFittingView(image: UIImage, width: CGFloat) -> CGSize {
+        let widthOfView = width
+        let heightOfView = width
+        let widthOfImage = image.size.width
+        let heightOfImage = image.size.height
+        
+        guard widthOfView != 0,
+              heightOfView != 0,
+              widthOfImage != 0,
+              heightOfImage != 0 else { return CGSize(width: 100, height: 100) }
+        
+        let ratio = widthOfView / widthOfImage
+
+        let newWidthOfImage = widthOfView - imageInsetsConstant * 2
+        let newHeightOfImage = heightOfImage * ratio - imageInsetsConstant * 2
+        
+        return CGSize(width: newWidthOfImage, height: newHeightOfImage)
+    }
+}
+
